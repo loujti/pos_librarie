be032fda946f3a70545ce5b0dd7babf6e697e212
loujti <intissarloujeyn@gmail.com>
2022-08-06 12:32:11 +0100

    poslibrairie

---
+++ custom_product/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 IBN
+
+
+{
+    'name': 'Product_Custom',
+    'description': """
+        Product_Custom""",
+    'version': '15.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'IBN',
+    'depends': [
+        # odoo addons
+        'point_of_sale',
+    ],
+    'data': [
+        'views/product_inherit.xml',
+    ],
+    'demo': [
+    ],
+}
