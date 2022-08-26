55fec0e07dc27c2c861dd622dd6079c9c24ae9b6
loujti <intissarloujeyn@gmail.com>
2022-08-26 20:41:56 +0100

    add list_price11

--- models/product_template_inherit.py
+++ models/product_template_inherit.py
@@ -9,12 +9,8 @@ from odoo import api, fields, models, tools, _, SUPERUSER_ID
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    list_price = fields.Float(
-        'Sales Price', default=1.0,
-        digits='Product Price', compute="_compute_price_ttc"
-    )
 
-    def _compute_price_ttc(self):
-        if self.list_price and self.taxes_id:
+    @api.onchange('list_price')
+    def _onchange_list_price(self):
+        if self.list_price:
             self.list_price = self.list_price + self.taxes_id.amount
-        return self.list_price
